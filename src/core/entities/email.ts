c6ef6066e58efc3a759d0659b16de3a712f7f565
tsmurer <tiagostaurenghi@gmail.com>
2023-11-20 16:54:07 -0300

    schema.prisma first version

--- src/core/entities/email.ts
+++ src/core/entities/email.ts
@@ -1,3 +1,16 @@
 export class Email {
-    
+    private address: String
+
+    constructor(address: String) {
+        this.address = address;
+    }
+
+    getAddress(): String {
+        return this.address;
+    }
+
+    isValidAddress(): Boolean {
+        let regex = new RegExp(`(?:[a-z0-9!#$%&'*+/=?^_\`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])`)
+        return regex.test(this.address.valueOf())
+    }
 }
--- src/core/entities/name.ts
+++ src/core/entities/name.ts
@@ -1,3 +1,9 @@
 export class Name {
+    private firstName: String;
+    private lastName: String;
 
+    constructor(firstName: String, lastName: String) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
